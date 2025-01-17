d3df75de23d9c25927af1c7c710e3a8c749424ce
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2025-01-17 11:47:18 +0530

    Setup Outlet and Layout

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-[#080C18] h-4/5 w-full text-white '>
+    <div className='bg-[#080C18] h-4/5 w-full text-white px-20 py-32'>
       <h1>Lorem, ipsum.</h1>
-      <h2>L</h2>
+      <h2>Lorem, ipsum dolor.</h2>
     </div>
   )
 }
