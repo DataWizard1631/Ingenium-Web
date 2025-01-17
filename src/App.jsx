5c4bed9629a83bac8fed7f345e67f9935e275744
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2025-01-17 11:30:19 +0530

    Setup Outlet and Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes,Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import Layout from './Components/Layout'
 
 function App() {
     return (
     <>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Layout/>}>
+        <Route path="" element={<Home/>}/>
+      </Route>
     </Routes>
     </>
   )
