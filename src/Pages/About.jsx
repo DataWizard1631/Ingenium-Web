67e145e3438a991ad85f813a7d56e48113781627
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2025-01-31 15:06:56 +0530

    Complete about us page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,69 +1,53 @@
-import React from 'react'
-
-import AboutCard from "../Components/AboutCard";
+import React from 'react';
+import AboutCard from '../Components/AboutCard';
 
 function About() {
   return (
-    <div className="bg-[url('/bg-texture.jpg')] bg-repeat bg-auto py-24 px-12 w-full flex flex-col gap-14">
-      {/* <InfoCard />
-      <MouseCard/> */}
-      <div>
-        <div className="w-full h-fit flex justify-center">
-          <div className="w-48 sm:w-56 md:w-64 mb-4 sm:mb-6 md:mb-8">
-            <img
-              src="/Logos/Ing_White_2025.png"
-              alt="Ingenium Logo"
-              className="w-full h-auto"
-            />
-          </div>
-        </div>
-        <div className="flex w-full h-1/3 justify-around px-12 py-5 gap-6">
-          <div className="w-1/2 h-96 text-white">
-            <p className="font-secFont1 text-2xl">about</p>
-            <p className="font-primaryFont text-5xl">INGENIUM</p>
-            <p className="font-secFont1 text-2xl text-justify pt-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Architecto temporibus tenetur facere. Repudiandae dolor voluptate
-              corrupti blanditiis, aperiam temporibus sunt illo omnis distinctio
-              in reiciendis, aliquid odit doloribus eius aliquam earum inventore
-              eum officia assumenda impedit, quaerat consectetur. Hic, sequi
-              tempore aliquam officiis aspernatur quo amet? Explicabo
-              praesentium placeat ullam? Ad voluptatem, laudantium dolore unde
-              eius in consequuntur! Labore quod nisi officia autem nemo neque
-              rem qui nihil cupiditate.
-            </p>
-          </div>
-          <img src="/public/3.jpg" className="h-96 w-72" />
+    <div className="bg-[url('/bg-texture.jpg')] bg-repeat bg-auto py-16 px-4 sm:px-8 lg:px-12 xl:px-24 w-full flex flex-col gap-10">
+      {/* Ingenium Logo */}
+      <div className="w-full flex justify-center">
+        <div className="w-32 pt-4 sm:w-40 md:w-48 lg:w-56 xl:w-64">
+          <img src="/Logos/Ing_White_2025.png" alt="Ingenium Logo" className="w-full h-auto" />
         </div>
       </div>
-      <div className="w-full h-3/4">
-        <p className="text-5xl font-primaryFont text-white text-center">
-          MEET OUR TEAM
-        </p>
-        <div className="grid grid-cols-1 py-6 px-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
+
+      {/* About Section */}
+      <div className="flex flex-col md:flex-row items-center justify-around px-4 sm:px-12 py-5 gap-6">
+        <div className="w-full md:w-1/2 text-white text-center md:text-left">
+          <p className="font-secFont1 text-xl sm:text-2xl">About</p>
+          <p className="font-primaryFont text-4xl sm:text-5xl">INGENIUM</p>
+          <p className="font-secFont1 text-base sm:text-lg md:text-xl text-justify pt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto temporibus
+            tenetur facere. Repudiandae dolor voluptate corrupti blanditiis, aperiam temporibus sunt
+            illo omnis distinctio in reiciendis, aliquid odit doloribus eius aliquam.
+          </p>
+        </div>
+        <img src="/3.jpg" className="h-72 sm:h-80 md:h-96 w-48 sm:w-64 md:w-72 rounded-lg object-cover" />
+      </div>
+
+      {/* Meet Our Team */}
+      <div className="w-full">
+        <p className="text-4xl sm:text-5xl font-primaryFont text-white text-center">MEET OUR TEAM</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-6 py-6 px-4 sm:px-8 lg:px-12">
+          {Array(6).fill(0).map((_, i) => (
+            <AboutCard key={i} name="Lorem ipsum" post="lorem" image="/1.jpg" />
+          ))}
         </div>
       </div>
-      <div className="w-full h-3/4">
-        <p className="text-7xl font-primaryFont text-center text-transparent bg-gradient-to-b from-white/50 to-gray-400 bg-clip-text">
+
+      {/* Heads Section */}
+      <div className="w-full">
+        <p className="text-5xl sm:text-6xl md:text-7xl font-primaryFont text-center text-transparent bg-gradient-to-b from-white/50 to-gray-400 bg-clip-text">
           HEADS
         </p>
-        <div className="max-w-screen-lg mx-auto grid grid-cols-1 py-6 px-6 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
-          <AboutCard name="Lorem ipsum" post="lorem" image="/public/1.jpg" />
+        <div className="max-w-screen-lg mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-6 py-6 px-4 sm:px-8">
+          {Array(6).fill(0).map((_, i) => (
+            <AboutCard key={i} name="Lorem ipsum" post="lorem" image="/1.jpg" />
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
