b14e7d65ee44625e9be9ca7b90e41339f10ae329
vishv0407 <vishvboda0407@gmail.com>
2025-03-08 19:30:41 +0530

    added photos, minor ui changes

--- src/Components/home/RecapCards.jsx
+++ src/Components/home/RecapCards.jsx
@@ -5,7 +5,7 @@ const RecapCards = () => {
     <div>
       <div>
       <h1 className='font-primaryFont text-white text-center text-4xl sm:text-5xl md:text-6xl font-bold mb-12 mt-48'>
-        Event Gallery
+        past Artists
       </h1>
     </div>
     <div className="container mx-auto px-4 h-[800px] md:h-[600px] lg:h-[800px] ">
@@ -13,7 +13,7 @@ const RecapCards = () => {
         {/* First column: 2x1 and 2x1 */}
         <div className="row-span-1 col-span-1 rounded-2xl overflow-hidden bg-gray-200">
           <img 
-            src="/Images/kenny.png" 
+            src="/Images/group.jpg" 
             alt="Image 1"
             className="w-full h-full object-cover"
           />
@@ -29,15 +29,15 @@ const RecapCards = () => {
         {/* Second column: 2x1, RECAP 1x1, 1x1 */}
         <div className="col-span-1 row-span-2 md:col-start-2 md:row-start-1 rounded-2xl overflow-hidden bg-gray-200">
           <img 
-            src="/Images/the-local-train.jpg" 
+            src="/Images/the-local-train.png" 
             alt="Image 3"
             className="w-full h-full object-fill"
           />
         </div>
-        <div className="col-start-2 row-start-3 row-span-1 rounded-2xl flex items-center justify-center">
+        {/* <div className="col-start-2 row-start-3 row-span-1 rounded-2xl flex items-center justify-center">
           <h2 className="text-7xl font-primaryFont font-bold text-white">RECAP</h2>
-        </div>
-        <div className="col-span-1 row-span-1 md:col-start-2 md:row-start-4 rounded-2xl overflow-hidden bg-gray-200">
+        </div> */}
+        <div className="col-span-1 row-span-2 md:col-start-2 md:row-start-3 rounded-2xl overflow-hidden bg-gray-200">
           <img 
             src="/Images/kenny.png" 
             alt="Imag 4"
@@ -55,7 +55,7 @@ const RecapCards = () => {
         </div>
         <div className="col-span-1 row-span-1 md:col-start-3 md:row-start-4 rounded-2xl overflow-hidden bg-gray-200">
           <img 
-            src="/Images/1.jpg" 
+            src="/Images/group2.png" 
             alt="Image 6"
             className="w-full h-full object-cover"
           />
--- src/Pages/SponsorPage.jsx
+++ src/Pages/SponsorPage.jsx
@@ -4,7 +4,7 @@ import { Sponsors } from '../Components/Sponsors/Sponsors'
 function SponsorPage() {
   return (
     <div className="bg-[url('/bg-texture.jpg')] bg-repeat bg-auto min-h-screen bg-black text-white">
-      <div className='w-full flex justify-center items-center'>
+      <div className='w-full flex justify-center items-center mt-20'>
       <div className="mt-[8vh] w-48 sm:w-56 md:w-64 mb-4 sm:mb-6 md:mb-8">
           <img
             src="/Logos/Ing_White_2025.png"
