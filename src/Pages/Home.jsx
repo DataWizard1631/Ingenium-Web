76289cbdfb0d20600b22be6c87d0bdeac4d1fed0
vishv0407 <vishvboda0407@gmail.com>
2025-03-08 16:16:18 +0530

    fix image folder structure

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react'
-import { Hero } from '../Components/Hero'
 import { AboutUs } from '../Components/AboutUs'
-import ImageGallary from '../Components/ImageGallary'
 import EventCarousel from '../Components/home/EventCarousel';
 import Sponsors from '../Components/Sponsors';
 import Temp from '../Components/home/Temp';
@@ -90,21 +88,23 @@ function Home() {
         <WhyJoinUs />
         <EventCarousel/>
 
+        <>
         <h1 className='font-primaryFont text-white text-center text-4xl sm:text-5xl md:text-6xl font-bold mb-12'>
             Sponsors
-          </h1>
+        </h1>
           
-          <div className="h-[250px] sm:h-[250px] w-full relative mb-32">
-            <Sponsors
-              items={logoImgs}
-              direction='horizontal'
-              pauseOnHover={true}
-              size='clamp(6rem, 7rem + 15vmin, 20rem)'
-              duration='60s'
-              bgColor='#060606'
-              bgAccentColor='#111111'
-            />
-          </div>
+        <div className="h-[250px] sm:h-[250px] w-full relative mb-32">
+          <Sponsors
+            items={logoImgs}
+            direction='horizontal'
+            pauseOnHover={true}
+            size='clamp(6rem, 7rem + 15vmin, 20rem)'
+            duration='60s'
+            bgColor='#060606'
+            bgAccentColor='#111111'
+          />
+        </div>
+        </>
         
     </div>
   )
