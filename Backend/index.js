8b994c6d95bd5aa419ffa6aac3951160d0a287f8
vishv0407 <vishvboda0407@gmail.com>
2025-03-29 10:37:52 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://ingenium2025.com', 'https://ingenium-web2.onrender.com'],
+  origin: ['http://localhost:5173', 'https://www.ingenium2025.com','https://ingenium2025.com', 'https://ingenium-web2.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
